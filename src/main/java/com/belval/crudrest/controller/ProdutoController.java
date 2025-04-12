2371955a9a64218ae5067887860d22feb20678c7
Erik <erik15casa@gmail.com>
2025-04-11 22:49:22 -0300

    novo commit

--- src/main/java/com/belval/crudrest/controller/ProdutoController.java
+++ src/main/java/com/belval/crudrest/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -66,7 +67,31 @@ public class ProdutoController {
 				.status(HttpStatus.CREATED)
 				.body(produto);
 		
+	}
+	
+//Observação:para métodos que não sejam o GET e o POST é necessário colocar o -X(menos xis maiúsculo)
+//curl -X PUT http://localhost:8080/produtos/1 -H "Content-Type: application/json; Charset=utf-8" -d @produto-mortadela2.json
+	
+	@PutMapping("/produtos/{id}")
+	public ResponseEntity<Object> atualizarProduto(
+			@PathVariable Integer id,
+			@RequestBody Produto prod) {
 		
+		Optional<Produto> produto = repository.findById(id);
+		
+		if(produto.isEmpty()) {
+			
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body("Produto não encontrado!");
+			}
+		
+		prod.setId(id);
+		repository.save(prod);
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body("Produto atualizado com sucesso!");		
 	}
 	
 	
